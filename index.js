84991ea17dbdcedaec3a1c9e28cb957d557498d9
Luís Campanholi <lpcampanholi@gmail.com>
2024-04-14 11:38:02 -0300

    parâmetros

--- index.js
+++ index.js
@@ -2,5 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.listen(8081);
-console.log("Servidor rodando");
+app.get("/", (req, res) => {
+  res.send("Seja bem-vindo ao meu app");
+});
+
+app.get("/:nome/:cargo", (req, res) => {
+  res.send(
+    `<h2>Nome: ${req.params.nome}</h2>` + `<h2>Cargo: ${req.params.cargo}</h2>`
+  );
+});
+
+app.get("/blog", (req, res) => {
+  res.send("Seja bem-vindo ao meu blog");
+});
+
+app.listen(8081, () => {
+  console.log("Servidor rodando na url http://localhost:8081");
+});
